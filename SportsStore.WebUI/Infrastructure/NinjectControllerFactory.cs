2a4fa1d3ea6c65b722be88d0c0db4be35fd7d838
t4rn <tarn@onet.eu>
2017-06-01 11:29:14 +0200

    working

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,56 @@
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+        protected override IController GetControllerInstance(RequestContext
+        requestContext, Type controllerType)
+        {
+            return controllerType == null
+            ? null
+            : (IController)ninjectKernel.Get(controllerType);
+        }
+        private void AddBindings()
+        {
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> {
+            //    new Product { Name = "Football", Price = 25 },
+            //    new Product { Name = "Surf board", Price = 179 },
+            //    new Product { Name = "Running shoes", Price = 95 }
+            //}.AsQueryable());
+            //ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager
+                .AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+
+            ninjectKernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
+
+            ninjectKernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
+        }
+    }
+}
